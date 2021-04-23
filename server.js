f27d1779ba0d0bb80c270a288edb753b311fbf62
Malgorzata Wladyka <mwladyka126@gmail.com>
2021-04-23 20:36:57 +0200

    add passport to server

--- server.js
+++ server.js
@@ -1,34 +1,83 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const hbs = require('express-handlebars');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const hbs = require("express-handlebars");
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("express-session");
 
 const app = express();
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
-app.set('view engine', '.hbs');
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "708326917535-e8ep05pdp6pre1mrv6kmbp6q0vso44jd.apps.googleusercontent.com",
+      clientSecret: "pCZ9gaAbOTuvm14HmRv5o5et",
+      callbackURL: "http://localhost:8000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+      console.log(profile);
+    }
+  )
+);
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: "anything" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine(
+  "hbs",
+  hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
+);
+app.set("view engine", ".hbs");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
+app.get("/user/logged", (req, res) => {
+  res.render("logged");
 });
 
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/user/no-permission" }),
+  (req, res) => {
+    res.redirect("/user/logged");
+  }
+);
+
+app.get("/user/no-permission", (req, res) => {
+  res.render("noPermission");
 });
 
-app.use('/', (req, res) => {
-  res.status(404).render('notFound');
+app.use("/", (req, res) => {
+  res.status(404).render("notFound");
 });
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+app.listen("8000", () => {
+  console.log("Server is running on port: 8000");
 });
